0e542dc8dfe84d5cb7fbeab7940667e66ad597c3
Donald Smith <donaldsmith1911@gmail.com>
2021-05-27 09:06:54 -0500

    updates

--- script.js
+++ script.js
@@ -1,19 +1,67 @@
-let button = document.createElement("button");
-// let btnText = document.createTextNode("Add Square");
-// button.appendChild(btnText);
-button.textContent = "Add Square";
-document.body.appendChild(button);
+document.addEventListener("DOMContentLoaded", function () {
+  let button = document.createElement("button");
+  // let btnText = document.createTextNode("Add Square");
+  // button.appendChild(btnText);
+  button.textContent = "Add Square";
 
-button.addEventListener("click", function(){
-    let square = document.createElement('div');
-    let squareContainer = document.createElement('div');
-    document.body.appendChild(squareContainer);
-    square.className = 'square';
+  let squareContainer = document.createElement("div");
+  squareContainer.id = "square-Container";
 
-    square.style.border = '1px solid black';
-    square.style.background = 'black';
-    square.style.width = '3em';
-    square.style.height = '3em';
-    squareContainer.appendChild(square);
-    document.body.appendChild(square);
+  let squareCounter = 0;
+  const squareColors = [
+    "red",
+    "blue",
+    "yellow",
+    "orange",
+    "brown",
+    "purple",
+    "green",
+    "pink",
+  ];
+
+  button.addEventListener("click", function () {
+    squareCounter++;
+
+    let square = document.createElement("div");
+    square.className = "square";
+    square.id = squareCounter;
+
+    square.style.border = "1px solid black";
+    square.style.background = "black";
+    square.style.width = "3em";
+    square.style.height = "3em";
+
+    square.addEventListener("mouseenter", function (e) {
+      e.target.textContent = square.id;
+    });
+
+    square.addEventListener("mouseleave", function (e) {
+      e.target.textContent = "";
+    });
+
+    square.addEventListener("click", function () {
+      let randNum = Math.floor(Math.random() * squareColors.length);
+      square.style.backgroundColor = squareColors[randNum];
+    });
+
+
+  square.addEventListener("dblclick", function () {
+    if (square.id % 2 == 0) {
+      if (square.nextSibling) {
+        square.nextSibling.remove();
+      } else {
+        alert("No Square!!");
+      }
+    } else {
+      if (square.previousSibling) {
+        square.previousSibling.remove();
+      } else {
+        alert("No Square");
+      }
+    }
+  });
+  squareContainer.appendChild(square);
+});
+  document.body.appendChild(button);
+  document.body.appendChild(squareContainer);
 });
